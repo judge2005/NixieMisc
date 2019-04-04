efe2255dc5163fdd939039774d63ca2c827d12a2
Paul Andrews <judge2005@gmail.com>
2019-04-04 17:39:43 -0400

    Added movement sensor class

---
+++ MovementSensor.h
@@ -0,0 +1,49 @@
+/*
+ * MovementSensor.h
+ *
+ *  Created on: Mar 29, 2019
+ *      Author: mpand
+ */
+
+#ifndef LIBRARIES_NIXIEMISC_MOVEMENTSENSOR_H_
+#define LIBRARIES_NIXIEMISC_MOVEMENTSENSOR_H_
+
+#include <arduino.h>
+
+class MovementSensor {
+public:
+	MovementSensor(const byte pin) : onTime(0), delayMs(0), pin(pin) {
+	}
+
+	void setDelay(byte delayMinutes) {
+		delayMs = delayMinutes * 60000;
+	}
+
+	void setOnTime(unsigned long onTime) {
+		this->onTime = onTime;
+	}
+
+	bool isOn() {
+		return !isOff();
+	}
+
+	bool isOff() {
+		int state = digitalRead(pin);
+
+		unsigned long nowMs = millis();
+
+		if (state) {
+			onTime = nowMs;	// Sensor will stay high while movement is detected
+		}
+
+		return (delayMs != 0) && (nowMs - onTime >= delayMs);	// So zero = always off
+	}
+
+private:
+	unsigned long onTime;
+	unsigned long delayMs;
+
+	const byte pin;
+};
+
+#endif /* LIBRARIES_NIXIEMISC_MOVEMENTSENSOR_H_ */
