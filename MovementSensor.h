4f7938b23559b790bb73328cbd196f0107c576dc
Paul Andrews <judge2005@gmail.com>
2019-04-25 20:40:59 -0400

    Add remote movement sensor syncing

--- MovementSensor.h
+++ MovementSensor.h
@@ -12,15 +12,23 @@
 
 class MovementSensor {
 public:
-	MovementSensor(const byte pin) : onTime(0), delayMs(0), pin(pin) {
+	MovementSensor(const byte pin) : onTime(0), delayMs(0), src(1), pin(pin), callback(0) {
 	}
 
 	void setDelay(byte delayMinutes) {
 		delayMs = delayMinutes * 60000;
 	}
 
+	void setSrc(byte src) {
+		this->src = src;
+	}
+
 	void setOnTime(unsigned long onTime) {
 		this->onTime = onTime;
+	}
+
+	void setCallback(void (*callback)()) {
+		this->callback = callback;
 	}
 
 	bool isOn() {
@@ -28,22 +36,34 @@ public:
 	}
 
 	bool isOff() {
-		int state = digitalRead(pin);
-
 		unsigned long nowMs = millis();
 
-		if (state) {
-			onTime = nowMs;	// Sensor will stay high while movement is detected
+		if (src == 1) {	// Local device
+			int state = digitalRead(pin);
+
+
+			if (state) {
+				onTime = nowMs;	// Sensor will stay high while movement is detected
+				if (callback != 0) {
+					(*callback)();
+				}
+			}
 		}
 
-		return (delayMs != 0) && (nowMs - onTime >= delayMs);	// So zero = always off
+		return (src != 0) && (delayMs != 0) && (nowMs - onTime >= delayMs);	// So zero = always off
+	}
+
+	void trigger() {
+		onTime = millis();	// Sensor will stay high while movement is detected
 	}
 
 private:
 	unsigned long onTime;
 	unsigned long delayMs;
+	byte src;
 
 	const byte pin;
+	void (*callback)(void);
 };
 
 #endif /* LIBRARIES_NIXIEMISC_MOVEMENTSENSOR_H_ */
