865cf0ffb918719e9fa3d0fa33c7c8bed38f7fae
Paul Andrews <judge2005@gmail.com>
2024-02-17 21:49:12 -0500

    Fix heap usage and free scketch space reporting for ESP32

--- WSInfoHandler.cpp
+++ WSInfoHandler.cpp
@@ -8,6 +8,28 @@
 #include <Uptime.h>
 #include <ArduinoJson.h>
 #include <AsyncWebSocket.h>
+extern "C" {
+#include "esp_ota_ops.h"
+#include "esp_image_format.h"
+#include "esp_heap_caps.h"
+}
+
+static uint32_t sketchSize(sketchSize_t response) {
+    esp_image_metadata_t data;
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    if (!running) return 0;
+    const esp_partition_pos_t running_pos  = {
+        .offset = running->address,
+        .size = running->size,
+    };
+    data.start_addr = running_pos.offset;
+    esp_image_verify(ESP_IMAGE_VERIFY, &running_pos, &data);
+    if (response) {
+        return running_pos.size - data.image_len;
+    } else {
+        return data.image_len;
+    }
+}
 
 void WSInfoHandler::handle(AsyncWebSocketClient *client, char *data) {
 	cbFunc();
@@ -32,10 +54,21 @@ void WSInfoHandler::handle(AsyncWebSocketClient *client, char *data) {
 
 #ifndef ESP32
 	value["esp_boot_version"] = ESP.getBootVersion();
-#endif
 	value["esp_free_heap"] = ESP.getFreeHeap();
 	value["esp_sketch_size"] = ESP.getSketchSize();
 	value["esp_sketch_space"] = ESP.getFreeSketchSpace();
+#else
+	size_t freeHeap = ESP.getFreeHeap();
+	size_t free8bitHeap = heap_caps_get_free_size(MALLOC_CAP_8BIT);
+	size_t freeIRAMHeap = freeHeap - free8bitHeap;
+
+	value["esp_free_heap"] = free8bitHeap;
+	value["esp_sketch_size"] = sketchSize(SKETCH_SIZE_TOTAL);
+	value["esp_sketch_space"] = sketchSize(SKETCH_SIZE_FREE);
+	value["esp_free_iram_heap"] = freeIRAMHeap;
+	value["esp_free_heap_min"] = heap_caps_get_minimum_free_size(MALLOC_CAP_8BIT);
+	value["esp_max_alloc_heap"] = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
+#endif
 #ifndef ESP32
 	value["esp_flash_size"] = ESP.getFlashChipRealSize();
 	value["esp_chip_id"] = String(ESP.getChipId(), HEX);
